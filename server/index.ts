ae5855dcec4b4f378df0c181f9792eebc9b8be4d
Daniel Treviño <danielivert@gmail.com>
2019-06-20 15:13:09 +0200

    Remove react express views

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,6 @@ import typeDefs from './schema/schema'
 import resolvers from './resolvers/resolvers'
 import { PORT } from './utils/constants'
 import models from './models/models'
-import { createEngine } from 'express-react-views'
 
 const server = new ApolloServer({
   context: request => ({
@@ -20,19 +19,11 @@ const server = new ApolloServer({
 })
 
 const app = express()
-// Render react from SSR - Account verified view
-app.set('views', __dirname + '/views')
-app.set('view engine', 'js')
-app.engine('js', createEngine())
 
 app.use(bodyParser.json())
 
 app.get('/status', (_: any, res: any) => {
   res.json({ data: `${Date.now()}` })
-})
-
-app.get('/verify/:id', (_req: any, res: any) => {
-  res.render('AccountVerified', { msg: 'Verified!' })
 })
 
 app.post('/data', (req, res) => {
--- server/views/AccountVerified.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-
-const AccountVerified = props => <div>Message: {props.msg}</div>
-
-export default AccountVerified
