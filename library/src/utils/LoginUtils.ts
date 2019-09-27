fc7a6b7f31e4d19d18b4b1555b26e247c8961e08
Daniel Treviño <danielivert@gmail.com>
2019-09-27 21:29:31 +0200

    Save login cookie on the root path

--- library/src/utils/LoginUtils.ts
+++ library/src/utils/LoginUtils.ts
@@ -2,7 +2,8 @@ import cookie from 'cookie'
 
 export const createTokenCookie = (token: string) => {
   document.cookie = cookie.serialize('loginplify-token', token, {
-    maxAge: 30 * 24 * 60 * 60 // 30 days
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    path: '/'
   })
 }
 
