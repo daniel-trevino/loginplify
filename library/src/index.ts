4455660d35bfd8c6898328777a3691974379c256
Daniel Treviño <danielivert@gmail.com>
2019-11-29 09:19:08 +0100

    Export more components on library

--- library/src/index.ts
+++ library/src/index.ts
@@ -1,6 +1,12 @@
 // @ts-ignore
 global.fetch = require('node-fetch')
 import withAuthenticator from './components/withAuthenticator'
+import Balls from './components/Balls'
+import Button from './components/Button'
+import Input from './components/Input'
+import StringFormItem from './components/StringFormItem'
+import TextButton from './components/TextButton'
+import Message from './components/Message'
 import { useLoginServiceContext } from './context/UserContext'
 import NewPasswordPage from './pages/NewPasswordPage'
 import VerifyPage from './pages/VerifyPage'
@@ -9,7 +15,13 @@ export {
   withAuthenticator,
   useLoginServiceContext,
   NewPasswordPage,
-  VerifyPage
+  VerifyPage,
+  Balls,
+  Button,
+  Input,
+  Message,
+  StringFormItem,
+  TextButton
 }
 
 export default withAuthenticator
